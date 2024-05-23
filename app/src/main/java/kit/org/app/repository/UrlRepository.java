457f21f3700b2190bfac4eb55bf9ededf40bdaf2
kitdim <kitaykin9808@gmail.com>
2024-05-23 20:52:28 +0300

    remove unused imports

--- app/src/main/java/kit/org/app/repository/UrlRepository.java
+++ app/src/main/java/kit/org/app/repository/UrlRepository.java
@@ -2,7 +2,10 @@ package kit.org.app.repository;
 
 import kit.org.app.model.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UrlRepository extends JpaRepository<Url, Long> {
