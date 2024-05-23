f1a82633253bc85dd011eb4eca726fd976f2bc59
kitdim <kitaykin9808@gmail.com>
2024-05-23 20:52:50 +0300

    update method getUrlById

--- app/src/main/java/kit/org/app/service/UrlService.java
+++ app/src/main/java/kit/org/app/service/UrlService.java
@@ -1,6 +1,7 @@
 package kit.org.app.service;
 
 import kit.org.app.dto.url.CreateUrl;
+import kit.org.app.dto.url.ShowUrl;
 import kit.org.app.model.Url;
 import kit.org.app.repository.UrlRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,19 +18,28 @@ public class UrlService {
         return repository.findAll();
     }
 
-    public Url getUrlById(Long id) {
-        return repository.findById(id).orElseThrow(RuntimeException::new);
+    public ShowUrl getUrlById(Long id) {
+        Url someURl = repository.findById(id).orElseThrow(RuntimeException::new);
+        ShowUrl someShowUrl = new ShowUrl();
+
+        someShowUrl.setId(someURl.getId());
+        someShowUrl.setCreatedAt(someURl.getCreatedAt());
+        someShowUrl.setName(someURl.getName());
+
+        return someShowUrl;
     }
 
     public Url save(CreateUrl data) {
         Url url = new Url();
         url.setName(data.getUrl());
+
         return repository.save(url);
     }
 
     public Url update(Long id, Url data) {
-        Url someUrl = getUrlById(id);
+        Url someUrl = repository.findById(id).orElseThrow(RuntimeException::new);
         someUrl.setName(data.getName());
+
         return repository.save(someUrl);
     }
 
